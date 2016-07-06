f0b9f26c79d57ec797c760fce972dacfcee95399
nurzhanospanov <ospanovnurzhan@gmail.com>
2016-07-06 15:36:41 -0700

    initial commit

--- BMI Index Calculator/ViewController.swift
+++ BMI Index Calculator/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightField: UITextField!
+    
+    
+    
+    @IBOutlet weak var bmiIndexField: UITextView!
+    
+    @IBOutlet weak var categoryField: UITextView!
+   
+    @IBOutlet weak var heightField: UITextField!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +31,78 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func calculateButtonPressed(sender: AnyObject) {
+        
+        guard let weightFieldEntered = Double(weightField.text!) else {
+        
+            if weightField.text == "" {
+                
+                    let alert = UIAlertController(title: "Type in error", message: "Please type in your data.", preferredStyle: .Alert)
+                    
+                    presentViewController(alert, animated: true, completion:nil)
+            }
+                bmiIndexField.text = ""
+                return
+        }
+        
+        
+        guard let heightFieldEntered = Double(heightField.text!) else {
+            
+            if heightField.text == "" {
+            
+            let alert = UIAlertController(title: "Type in error", message: "Please type in your data.", preferredStyle: .Alert)
+            
+            presentViewController(alert, animated: true, completion:nil)
+            }
+
+            
+            weightField.text = ""
+
+            bmiIndexField.text = ""
+            return
+        }
+        
+
+        let heightPreparedForCalculation = (heightFieldEntered/100) * (heightFieldEntered/100)
+        
+        let bmiIndexResult = weightFieldEntered / heightPreparedForCalculation
+        
+        
+        bmiIndexField.text = String(format: "%.2f", bmiIndexResult)
+    
+        if bmiIndexResult < 15 {
+            
+            categoryField.text = "Very severely underweight" }
+                else if bmiIndexResult <= 16  {
+                    categoryField.text = "Severly Underweight"
+            }
+                else if bmiIndexResult <= 18.5  {
+                    categoryField.text = "Underweight"
+            }
+                else if bmiIndexResult <= 25  {
+                    categoryField.text = "Normal healthy weight"
+            }
+                else if bmiIndexResult <= 30  {
+                    categoryField.text = "Overweight"
+            }
+                else if bmiIndexResult <= 35  {
+                    categoryField.text = "Moderately obese"
+            }
+                else if bmiIndexResult <= 40  {
+                    categoryField.text = "Severely obese"
+            }
+                else if bmiIndexResult > 40  {
+                    categoryField.text = "Very severely obese"
+        }
+        
+            imageView.image = UIImage(named: ("image.png"))
+            
+        }
+        
+    }
 
 
-}
+
+
 
